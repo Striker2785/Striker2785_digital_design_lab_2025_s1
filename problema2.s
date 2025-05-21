d1e7c7c884c1b7c30d39a4af2acee8d2e0c018c4
Braulio Retana Murillo <64331077+Striker2785@users.noreply.github.com>
2025-05-20 20:20:11 -0600

    Solucion al problema 2

---
+++ problema2.s
@@ -0,0 +1,26 @@
+.data
+x:      .word 5           @ Puedes cambiar a 3, 5 o 6
+res:    .word 0
+
+.text
+.global _start
+
+_start:
+    LDR     R0, =x
+    LDR     R1, [R0]      @ x → R1
+    MOV     R2, #1        @ factorial acumulado → R2
+
+    CMP     R1, #0
+    BEQ     end
+
+loop:
+    MUL     R2, R2, R1
+    SUB     R1, R1, #1
+    CMP     R1, #0
+    BGT     loop
+
+    LDR     R0, =res
+    STR     R2, [R0]
+
+end:
+    B       end
